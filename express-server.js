4355564aafd6523fc367219da86fc6a541e56d69
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-09-14 15:30:20 +0000

    Added user registration, user specific-content, secure password storage, cookie encryption

--- express-server.js
+++ express-server.js
@@ -3,48 +3,67 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
 const cParser = require("cookie-parser");
+const cSession = require("cookie-session");
+
 app.use(cParser());
+app.use(cSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
 
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+const bcrypt = require('bcrypt');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    "userid": "userRandomID",
+    "longURL": "http://www.lighthouselabs.ca",
+  },
+  "9sm5xK": {
+    "userid": "user2RandomID",
+    "longURL": "http://www.google.com"
+  }
 };
 
 const users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    hashedPassword: '$2a$10$ESUkQ3Txnqtk3dNcaFfs0eHlyiG5Kk3HvH7rgETCBeUFOAC9sKsJe',
   },
  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    hashedPassword: '$2a$10$xbzvUSoZNlQ5/N0tXpegMebVIVC5V/A.lC0rP.ro5.AFX9tV1jhW.',
   }
 }
 
-// Stores user emails in a seprate array
-const userEmails = [];
-for (let user in users) {
-  userEmails.push(users[user].email);
+// Creates list of user emails currently in database
+function getUserEmails() {
+  const userEmails = [];
+  for (let user in users) {
+    userEmails.push(users[user].email);
+  }
+  return userEmails;
 }
 
 // Checks user email array to verify whether a given email address is already registered
 function checkUserEmail (givenEmail) {
-  for (let email of userEmails) {
+  const allEmails = getUserEmails();
+  for (let email of allEmails) {
     if (email === givenEmail) {
       return true;
     }
-    return false;
   }
+  return false;
 }
 
+// Locates a user based on a given email
 function locateUser (givenEmail) {
   for (let user in users) {
     if (users[user].email === givenEmail) {
@@ -61,9 +80,8 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    user_id: req.cookies["user_id"]
+    user_id: req.session["user_id"]
   }
-  console.log(req.cookies)
   res.render("urls_index", templateVars);
 })
 
@@ -72,49 +90,71 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    user_id: req.cookies["user_id"]
-  };
-  res.render("urls_new",templateVars);
+  if (!req.session["user_id"]) {
+    res.status(403);
+    res.end("403: You do not have permission to access this page.");
+  } else {
+    let templateVars = {
+      user_id: req.session["user_id"]
+    };
+    res.render("urls_new",templateVars);
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
+  let urlUser = urlDatabase[req.params.id].userid;
+
   if (!urlDatabase[req.params.id]) { //Returns 404 error if given shortURL does not exist
-    res.end("Page does not exist.");
     res.status(404);
+    res.end("404: Page not found.");
+  } else if (!req.session["user_id"] || req.session["user_id"] !== urlUser) {
+    res.status(403);
+    res.end("403: You do not have permission to access this page.");
   } else {
     let templateVars = {
       shortURL: req.params.id,
-      original: urlDatabase[req.params.id],
-      user_id: req.cookies["user_id"]
+      original: urlDatabase[req.params.id].longURL,
+      user_id: req.session["user_id"]
     };
     res.render("urls_show", templateVars);
   }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  let urlUser = urlDatabase[req.params.id].userid;
+  if (!req.session["user_id"] || req.session["user_id"] !== urlUser) {
+    res.status(403);
+    res.end("403: You do not have permission to perform this action.");
+  } else {
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls/:id/update", (req, res) => {
-  console.log(req.body);
-  newURL = req.body.longURL;
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect("/urls");
+  let urlUser = urlDatabase[req.params.id].userid;
+  if (!req.session["user_id"] || req.session["user_id"] !== urlUser) {
+    res.status(403);
+    res.end("403: You do not have permission to perform this action.");
+  } else {
+    newURL = req.body.longURL;
+    urlDatabase[req.params.id].longURL = req.body.longURL;
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
   var shortURL = generateRandomString(7);
-  urlDatabase[shortURL] = req.body.longURL;  // debug statement to see POST parameters
+  urlDatabase[shortURL] = {};
+  urlDatabase[shortURL].userid = req.session["user_id"];
+  urlDatabase[shortURL].longURL = req.body.longURL; // debug statement to see POST parameters
   res.redirect(`/urls/${shortURL}`);
   res.status(302);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  let destination = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(destination);
   res.status(302);
 });
 
@@ -124,20 +164,17 @@ app.get("/hello", (req, res) => {
 
 app.get("/login", (req, res) => {
   let templateVars = {
-    user_id: users[req.cookies["user_id"]]
+    user_id: users[req.session["user_id"]]
   };
   res.render("login",templateVars);
 });
 
 app.post("/login", (req, res) => {
-  givenEmail = req.body.email;
-  givenPassword = req.body.password;
-  givenUser = locateUser(givenEmail);
-  console.log(givenUser);
+  let givenEmail = req.body.email;
+  let givenUser = locateUser(givenEmail);
 
-  if ( givenUser && users[givenUser].password === givenPassword) {
-    res.cookie('user_id',users[givenUser].id);
-    console.log(givenUser);
+  if ( givenUser && bcrypt.compareSync(req.body.password, users[givenUser].hashedPassword) ) {
+    req.session.user_id = users[givenUser].id;
     res.redirect("/urls");
     res.status(302);
   } else {
@@ -148,7 +185,8 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  res.clearCookie("session");
+  res.clearCookie("session.sig");
   res.redirect("/urls");
   res.status(302);
 });
@@ -160,18 +198,17 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   let newUserID = generateRandomString(12);
   users[newUserID] = {};
-  console.log(req.body);
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
 
-  if ( !req.body.email || !req.body.password || checkUserEmail(req.body.email) ) {
+  if ( !req.body.email || !hashedPassword || checkUserEmail(req.body.email) ) {
     res.status(400);
     res.end("400: Bad Request \n Please enter a valid email/password that is not already registered");
   } else {
     users[newUserID]["id"] = newUserID;
     users[newUserID]["email"] = req.body.email;
-    users[newUserID]["password"] = req.body.password;
-    res.cookie('user_id',newUserID);
+    users[newUserID]["hashedPassword"] = hashedPassword;
+    req.session.user_id = newUserID;
     res.redirect('/urls');
-    console.log(users);
   }
 });
 
@@ -188,6 +225,3 @@ function generateRandomString(length) {
   }
   return text;
 }
-
-
-console.log(userEmails);
